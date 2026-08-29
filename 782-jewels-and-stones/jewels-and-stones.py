2030789e3d5e24fa876c5bdfa09f6acf80c3a92a
Pawadi Piragond Kanal <kanalpawadi@gmail.com>
2026-08-29 09:28:32 +0530

    Time: 7 ms (1.68%) | Memory: 12.3 MB (89.49%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        jewels_set=set(jewels)
+        count=0
+        for ch in stones:
+            if ch in jewels_set:
+                count+=1
+                print(ch)
+        
+        return count
+        
